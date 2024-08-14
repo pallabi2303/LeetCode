d075c9e528990f1ccf78dd0a4a66a3134d4ddcc5
Pallabi Pati <116326163+pallabi2303@users.noreply.github.com>
2024-08-15 00:04:45 +0530

    Time: 0 ms (100.00%), Space: 41.2 MB (77.62%) - LeetHub

--- 0872-leaf-similar-trees/0872-leaf-similar-trees.java
+++ 0872-leaf-similar-trees/0872-leaf-similar-trees.java
@@ -1,18 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-    public boolean leafSimilar(TreeNode root1, TreeNode root2) {
-        List<Integer> leaves1 = new ArrayList();
-        List<Integer> leaves2 = new ArrayList();
-        dfs(root1, leaves1);
-        dfs(root2, leaves2);
-        return leaves1.equals(leaves2);
-    }
-
-    public void dfs(TreeNode node, List<Integer> leafValues) {
-        if (node != null) {
-            if (node.left == null && node.right == null)
-                leafValues.add(node.val);
-            dfs(node.left, leafValues);
-            dfs(node.right, leafValues);
+    public static ArrayList<Integer> findLeaf(TreeNode root1,ArrayList<Integer> list1){
+        if(root1==null){
+            return list1;
         }
+        if(root1.left==null & root1.right==null){
+            list1.add(root1.val);
+        }
+    findLeaf(root1.left,list1);
+    findLeaf(root1.right,list1);
+    return list1;
+        
+        
+        
+    }
+    public boolean leafSimilar(TreeNode root1, TreeNode root2) {
+        ArrayList<Integer> list1=new ArrayList();
+        ArrayList<Integer> list2=new ArrayList();
+    findLeaf(root1,list1);
+        findLeaf(root2,list2);
+        return list1.equals(list2);
     }
 }
